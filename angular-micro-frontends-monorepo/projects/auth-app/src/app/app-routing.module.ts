4d49ffbddf8ffaa32a41c13bbedbe79d50bd4379
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-11-10 09:51:31 +0200

    App Component has router-outlet & routes updated

--- angular-micro-frontends-monorepo/projects/auth-app/src/app/app-routing.module.ts
+++ angular-micro-frontends-monorepo/projects/auth-app/src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  {component: LoginComponent, path: '/login'},
-  {component: RegisterComponent, path: '/register'},
+  {component: LoginComponent, path: 'login'},
+  {component: RegisterComponent, path: 'register'},
 ];
 
 @NgModule({
