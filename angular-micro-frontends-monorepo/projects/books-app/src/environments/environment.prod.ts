70d75cdff220206e8d40afd46e22083a3355cfe6
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-11-11 15:37:10 +0200

    logging in production is disabled using service

--- angular-micro-frontends-monorepo/projects/books-app/src/environments/environment.prod.ts
+++ angular-micro-frontends-monorepo/projects/books-app/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  booksUrl: 'http://localhost:9090/api/v1/books/',
+  searchBooksUrl: 'http://localhost:9090/api/v1/books?search=',
+
+  myBooksUrl: 'http://localhost:9090/api/v1/my-books/',
+  wishlistUrl: 'http://localhost:9090/api/v1/wishlist/',
 };
